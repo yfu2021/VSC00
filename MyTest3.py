f5f130d30240f99c02c4066187fa9c8e9709d583
yfu2021 <91210889+yfu2021@users.noreply.github.com>
2022-10-27 15:28:39 -0400

    Add files via upload

---
+++ MyTest3.py
@@ -0,0 +1,61 @@
+for x in "banana":
+  print(x)
+
+
+a = "Hello, World!"
+print(len(a))
+
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#########################################################################
+###Next, will pull bac to local repository and add more Python Codes ####
+#########################################################################
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+txt = "The best thing in life is free"
+##print('"Expensive"  not in txt')
+if "Expensive"  not in txt:
+    print('  "Expensive" is not in txt    ')
+
+
+b = "Hello, World!"
+print(b[2:5])
+print(b[:5])
+b = "Hello, World!"
+print(b[-6:-2])
+
+
+a = 200
+b = 33
+
+if b >a:
+    print('b is bigger than a')
+else:
+    print(" b is smaller than a")
+
+
+##bool(123)
+
+print(bool(0))
+
+#############
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+print(bool(("","2","3")))
+print(bool(["","2","3"]))
+print(bool({"","2","3"}))
+
+
+## Save my current work, and push it to the github repository.
